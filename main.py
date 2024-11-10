36009ae9d4fb649d26758ab67958f1458503052f
AskCoder <107125023+AskCoderr@users.noreply.github.com>
2024-11-10 16:03:13 +0530

    adding collision and bouncing mechanics, has bug when hitting paddle edge

--- main.py
+++ main.py
@@ -1,46 +1,148 @@
 import tkinter as tk
+
 root = tk.Tk()
 root.title("Ping Pong Game")
 root.resizable(False, False)
 root.geometry("600x400")
-canvas = tk.Canvas(root, width=600, height=400, bg="black")
-canvas.pack()
-paddle1 = canvas.create_rectangle(10, 150, 30, 250, fill="white")
-paddle2 = canvas.create_rectangle(570, 150, 590, 250, fill="white")
-ball = canvas.create_oval(290, 190, 310, 210, fill="red")
-player1_score = 0
-player2_score = 0
-x_velocity = 100 # im considering the unit as pixel/second
-y_velocity = 100
-scoredisplay = canvas.create_text(300, 50, text=f"{player1_score} - {player2_score}", fill="yellow", font=("Helvetica", 24))
-canvas.itemconfig(scoredisplay, text=f"{player1_score} - {player2_score}")
-ps=20
-
-def moveup1(event):
-    if canvas.coords(paddle1)[1] > 0:
-        canvas.move(paddle1, 0, -ps)
-def movedown1(event):
-    if canvas.coords(paddle1)[3] < 400:
-        canvas.move(paddle1, 0, ps)
-def moveup2(event):
-    if canvas.coords(paddle2)[1] > 0:
-        canvas.move(paddle2, 0, -ps)
-
-def movedown2(event):
-    if canvas.coords(paddle2)[3] < 400:
-        canvas.move(paddle2, 0, ps)
 
 
-# when calling this function pass the parameter w if ball collides with wall, p if colliding with paddle
-def update_velocity(x):
-    if x == "w":
-        y_velocity *= -1
-    elif x == "p":
-        x_velocity *= -1
-        
+class GUIandLogic():
+    def __init__(self, main_loop):
+        self.canvas = tk.Canvas(root, width=600, height=400, bg="black")
+        self.canvas.pack()
+        self.paddle1 = self.canvas.create_rectangle(10,
+                                                    150,
+                                                    30,
+                                                    250,
+                                                    fill="white")
+        self.paddle2 = self.canvas.create_rectangle(570,
+                                                    150,
+                                                    590,
+                                                    250,
+                                                    fill="white")
+        self.ball = self.canvas.create_oval(290, 190, 310, 210, fill="red")
+        self.player1_score = 0
+        self.player2_score = 0
+        self.x_velocity = 0.3  # im considering the unit as pixel/10 ms
+        self.y_velocity = 0.3
+        self.scoredisplay = self.canvas.create_text(
+            300,
+            50,
+            text=f"{self.player1_score} - {self.player2_score}",
+            fill="yellow",
+            font=("Helvetica", 24))
+        self.canvas.itemconfig(
+            self.scoredisplay,
+            text=f"{self.player1_score} - {self.player2_score}")
+        self.ps = 20
+        self.paddle1top = [
+            self.canvas.coords(self.paddle1)[2],
+            self.canvas.coords(self.paddle1)[1]
+        ]
+        self.paddle1bottom = [
+            self.canvas.coords(self.paddle1)[2],
+            self.canvas.coords(self.paddle1)[3]
+        ]
+        self.paddle2top = [
+            self.canvas.coords(self.paddle2)[0],
+            self.canvas.coords(self.paddle2)[1]
+        ]
+        self.paddle2bottom = [
+            self.canvas.coords(self.paddle2)[0],
+            self.canvas.coords(self.paddle2)[3]
+        ]
+        self.ballMiddleLeft = [
+            self.canvas.coords(self.ball)[0],
+            self.canvas.coords(self.ball)[3] - ((self.canvas.coords(self.ball)[3] - self.canvas.coords(self.ball)[1])/2)
+        ]
+        self.ballMiddleRight = [
+            self.canvas.coords(self.ball)[2],
+            self.canvas.coords(self.ball)[3] - ((self.canvas.coords(self.ball)[3] - self.canvas.coords(self.ball)[1])/2)
+        ]
+        self.ballTopMiddle = [
+            self.canvas.coords(self.ball)[2] - ((self.canvas.coords(self.ball)[2] - self.canvas.coords(self.ball)[0])/2),
+            self.canvas.coords(self.ball)[1]
+        ]
+        self.ballBottomMiddle = [
+            self.canvas.coords(self.ball)[2] - ((self.canvas.coords(self.ball)[2] - self.canvas.coords(self.ball)[0])/2),
+            self.canvas.coords(self.ball)[3]
+        ]
+        self.update_ball_position()
+        self.collision()
 
-root.bind("w", moveup1)
-root.bind("s", movedown1)
-root.bind('<Up>',moveup2)
-root.bind('<Down>',movedown2)
+    def moveup1(self, event):
+        if self.canvas.coords(self.paddle1)[1] > 0:
+            self.canvas.move(self.paddle1, 0, -self.ps)
+            self.update_paddle_points()
+
+    def movedown1(self, event):
+        if self.canvas.coords(self.paddle1)[3] < 400:
+            self.canvas.move(self.paddle1, 0, self.ps)
+            self.update_paddle_points()
+
+    def moveup2(self, event):
+        if self.canvas.coords(self.paddle2)[1] > 0:
+            self.canvas.move(self.paddle2, 0, -self.ps)
+            self.update_paddle_points()
+
+    def movedown2(self, event):
+        if self.canvas.coords(self.paddle2)[3] < 400:
+            self.canvas.move(self.paddle2, 0, self.ps)
+            self.update_paddle_points()
+
+    # when calling this function pass the parameter w if ball collides with wall, p if colliding with paddle
+    def update_velocity(self, x):
+        global y_velocity, x_velocity
+        if x == "w":
+            self.y_velocity *= -1
+        elif x == "p":
+            self.x_velocity *= -1
+
+    def update_paddle_points(self):
+        self.paddle1top = [self.canvas.coords(self.paddle1)[2], self.canvas.coords(self.paddle1)[1]]
+        self.paddle1bottom = [self.canvas.coords(self.paddle1)[2], self.canvas.coords(self.paddle1)[3]]
+        self.paddle2top = [self.canvas.coords(self.paddle2)[0], self.canvas.coords(self.paddle2)[1]]
+        self.paddle2bottom = [self.canvas.coords(self.paddle2)[0], self.canvas.coords(self.paddle2)[3]]
+
+    def update_ball_points(self):
+        self.ballMiddleLeft = [
+            self.canvas.coords(self.ball)[0],
+            self.canvas.coords(self.ball)[3] - ((self.canvas.coords(self.ball)[3] - self.canvas.coords(self.ball)[1])/2)
+        ]
+        self.ballMiddleRight = [
+            self.canvas.coords(self.ball)[2],
+            self.canvas.coords(self.ball)[3] - ((self.canvas.coords(self.ball)[3] - self.canvas.coords(self.ball)[1])/2)
+        ]
+        self.ballTopMiddle = [
+            self.canvas.coords(self.ball)[2] - ((self.canvas.coords(self.ball)[2] - self.canvas.coords(self.ball)[0])/2),
+            self.canvas.coords(self.ball)[1]
+        ]
+        self.ballBottomMiddle = [
+            self.canvas.coords(self.ball)[2] - ((self.canvas.coords(self.ball)[2] - self.canvas.coords(self.ball)[0])/2),
+            self.canvas.coords(self.ball)[3]
+        ]
+
+    def update_ball_position(self):
+        self.canvas.move(self.ball, self.x_velocity * 10, self.y_velocity * 10)
+        self.update_ball_points()
+        root.after(10, self.update_ball_position)
+
+    def collision(self):
+        if (self.ballMiddleLeft[0] < 0) or (self.ballMiddleRight[0] > 600):
+            self.canvas.coords(self.ball, 290, 190, 310, 210)
+        elif (self.ballTopMiddle[1] < 0) or (self.ballBottomMiddle[1] > 400):
+            self.update_velocity("w")
+        elif (self.ballMiddleLeft[0] <= self.paddle1bottom[0]
+              and self.paddle1top[1]<=self.ballMiddleLeft[1]<=self.paddle1bottom[1]):
+            self.update_velocity("p")
+        elif (self.ballMiddleRight[0] >= self.paddle2top[0] and self.paddle2top[1]<=self.ballMiddleRight[1]<=self.paddle2bottom[1]):
+            self.update_velocity("p")
+        root.after(10, self.collision)
+
+
+run = GUIandLogic(root)
+root.bind("w", run.moveup1)
+root.bind("s", run.movedown1)
+root.bind('<Up>', run.moveup2)
+root.bind('<Down>', run.movedown2)
 root.mainloop()
